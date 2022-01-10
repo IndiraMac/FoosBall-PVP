7d70874ece55c4a15be930dd81efa9b97241421f
Indira kar <indira@girzla.co.in>
2022-01-10 23:24:59 +0530

    Credit Updated

---
+++ foosball-master/Assets/Scripts/Credit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credit : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject creditMenu;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            mainMenu.SetActive(false);
+            creditMenu.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            creditMenu.SetActive(false);
+            mainMenu.SetActive(true);
+        }
+    }
+}
